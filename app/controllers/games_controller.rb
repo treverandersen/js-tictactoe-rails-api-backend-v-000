bb6a83842f561b98850df6c993ebaf3594c3aa85
treverandersen <trever.r.andersen@gmail.com>
2019-09-26 12:44:33 -0500

    setup our model, controller actions, and serializer for our JSON:API serialization scheme

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,34 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   
-  def index
-  end
+  before_action :set_game, only: [:show, :update]
 
-  def create
+  def index
+    games = Game.all
+    render json: games
   end
 
   def show
+    render json: @game
+  end
+
+  def create
+    game = Game.create(game_params)
+    render json: game, status: 201
   end
 
   def update
+    @game.update(game_params)
+    render json: @game
+  end
+
+  private
+
+  def game_params
+    params.permit(state: [])
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
